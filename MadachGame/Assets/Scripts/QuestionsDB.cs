d4d494d8a6088e8d17cca39d48f990b4718adce6
AdamKormos <49873113+AdamKormos@users.noreply.github.com>
2021-11-22 15:35:25 +0100

    Added last questions

--- MadachGame/Assets/Scripts/QuestionsDB.cs
+++ MadachGame/Assets/Scripts/QuestionsDB.cs
@@ -48,7 +48,32 @@ public static class QuestionsDB
         AddQuestion(7, "Mi lesz Évából a színben?", 1, new string[] { "Hadvezér", "Apáca", "Családanya", "Tanító" });
         AddQuestion(7, "Mit gondol Ádám az itt bemutatott keresztény egyházról?", 3, new string[] { "Támogatja", "Elítéli, mert más vallásban hisz", "Semlegesen áll hozzá", "Elítéli, mert elkorcsosult" });
 
+        AddQuestion(8, "Mit kell Ádámnak csinálnia a csillagászat helyett?", 2, new string[] { "Pásztorkodás", "Cigánykártya", "Horoszkóptudományok", "MBTI-típusok kutatása" });
+        AddQuestion(8, "Kinek a bőrébe bújik Ádám?", 0, new string[] { "Kepler", "Kopernikusz", "Newton", "Galileo" });
+        AddQuestion(8, "Kije ebben a színben Éva Ádámnak?", 3, new string[] { "Rabszolga", "Munkatárs", "Barát", "Feleség" });
 
+        AddQuestion(9, "Hogy kezdődik a szín?", 2, new string[] { "Lucifer átvezeti őt", "Megunja az előző helyszínt", "Elalszik az előző színben", "Elkábítja Éva" });
+        AddQuestion(9, "Mik az első szavai?", 0, new string[] { "Egyenlőség, szabadság, testvériség!", "Egy darab mennyország szállt le a vérpadra.", "Pokolnak gőze undorítja el.", "Kétséges rang -e hát szellem, tudás?" });
+
+        AddQuestion(10, "Mit gondol Ádám az eszmékről?", 2, new string[] { "Visszafejlődnek", "Állnak", "Fejlődnek" });
+        AddQuestion(10, "Hova tért vissza álmából Ádám?", 0, new string[] { "Prágába", "Egyiptomba", "Athénébe" });
+
+        AddQuestion(11, "Madách ... ez a szín.", 1, new string[] { "múltja", "jelene", "jövője" });
+        AddQuestion(11, "Honnan szemlélődik Ádám?", 2, new string[] { "A Big Ben-ről", "A Buckingham Palace-ről", "A Tower-ről", "A Bandi-ról" });
+        AddQuestion(11, "Ki éli egyedül túl a szín végét?", 0, new string[] { "Éva", "Ádám", "Polgárok" });
+
+        AddQuestion(12, "Mi uralkodik?", 1, new string[] { "Szerelem", "Tudományok" });
+        AddQuestion(12, "Teret ad a helyszín az egyéniségnek?", 1, "Igen", "Nem");
+        AddQuestion(12, "Milyen eszmerendszer elemei figyelhetők meg ebben?", 0, "Utópista szocializmus", "Hedonizmus", "Liberalizmus");
+
+        AddQuestion(13, "Diadalmaskodik ebben a színben Lucifer?", 1, new string[] { "Igen", "Nem" });
+        AddQuestion(13, "Hogy jut új erőre Ádám?", 1, new string[] { "Éva szavaira", "A Föld szellemének bíztató hangjára" });
+
+        AddQuestion(14, "Segítettek az emberiségnek végezetül a tudományok?", 1, new string[] { "Igen", "Nem" });
+        AddQuestion(14, "Mi történt az emberiséggel?", 1, new string[] { "Győzedelmeskedtek", "Elállatiasodtak" });
+
+        AddQuestion(15, "Miért különleges ez a szín?", 2, new string[] { "történelmi", "Ádám Lucifer szerepét tölti be", "Keretszín" });
+        AddQuestion(15, "Hogyan végződik a mű?", 1, new string[] { "Lucifer elcsábítja Évát", "Éva teherbe esik, beteljesül az emberiség végzete" });
 
         AddQuestion(Memoriter, "Egészítsd ki: „Be van fejezve a nagy mű, igen. A ___ forog, az alkotó pihen.\"", 2, new string[] { "kép", "mű", "gép", "masina" });
         AddQuestion(Memoriter, "Egészítsd ki: „Hiányzik az összhangzó ___.\"", 1, new string[] { "érzelem", "értelem", "lételem", "életem" });
@@ -83,7 +108,7 @@ public static class QuestionsDB
 
     public static QuizQuestion GetQuestion()
     {
-        int sceneIndex = Random.Range(0, 8); // Temporary -- will be Random.Range(0, 17)
+        int sceneIndex = Random.Range(0, 17);
         if (usableQuestions[sceneIndex].Count == 0)
         {
             foreach (QuizQuestion q in questions[sceneIndex])
