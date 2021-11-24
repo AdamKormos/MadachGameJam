f087138098da5257a0f646826f3277942937e5a6
AdamKormos <49873113+AdamKormos@users.noreply.github.com>
2021-11-24 14:57:46 +0100

    Text resolution fix, cursor hide

--- MadachGame/Assets/Scripts/GameController.cs
+++ MadachGame/Assets/Scripts/GameController.cs
@@ -20,6 +20,7 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.visible = false;
         instance = this;
         instance.gameEvaluationText.transform.parent.gameObject.SetActive(false);
 
